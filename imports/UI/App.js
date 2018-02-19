3546cc51bad1a3de7873b43b3b6b9a9a4cb97d85
unknown <aseelaldallal@gmail.com>
2018-02-18 19:39:29 -0500

    Add subtitle and style

--- imports/UI/App.js
+++ imports/UI/App.js
@@ -8,7 +8,10 @@ class App extends Component {
   render() {
     return (
       <div>
-        <TitleBar title={this.props.title} />
+        <TitleBar
+          title={this.props.title}
+          subtitle="Created by Aseel Al Dallal"
+        />
         <div className="wrapper">
           <PlayerList players={this.props.players} />
           <AddPlayer score={0} />
--- imports/UI/TitleBar/TitleBar.js
+++ imports/UI/TitleBar/TitleBar.js
@@ -7,7 +7,9 @@ class TitleBar extends Component {
       <div className="title-bar">
         <div className="wrapper">
           <h1> {this.props.title}</h1>
-          {this.props.subtitle ? <h2> {this.props.subtitle}</h2> : null}
+          {this.props.subtitle ? (
+            <h2 className="title-bar__subtitle"> {this.props.subtitle}</h2>
+          ) : null}
         </div>
       </div>
     );
